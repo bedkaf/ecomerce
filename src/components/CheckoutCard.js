c4959c70c07fb22fb11fb3c9b1745eb54d5feffe
Hamilton Belayne Moquera Amud <hammosquera@uan.edu.co>
2022-04-17 18:12:33 -0500

    Iniciando Checktout page
    Se creo la estructura del checkout page y funciona, viendo estos avances  voy a crear un componente  checkout card

---
+++ src/components/CheckoutCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CheckoutCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default CheckoutCard;
---
+++ src/components/CheckoutPage.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import CheckoutCard from "./CheckoutCard";
+import Product from "./Product";
+import dataProducts from "../produc-data";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flex: 1,
+    padding: "2rem",
+  },
+}));
+
+const CheckoutPage = () => {
+
+  const classes = useStyles();
+
+  function FomrRow() {
+    return(
+      <>
+        {dataProducts.map((item) => (
+          <Grid item xs={12} sm={6} lg={4}  >
+            <Product key={item.id} product={item} />
+          </Grid>
+        ))}
+      </>
+    )
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} >
+
+        <Grid item xs={12} >
+          <Typography align='center' gutterBottom variant="h4" >
+            Shopping Cart
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={8} md={9} container spacing={2} >
+          <FomrRow />
+        </Grid>
+
+        <Grid item xs={12} ms={4} md={3} >
+          <Typography align="center" gutterBottom variant="h4" >
+            Total
+          </Typography>
+        </Grid>
+
+      </Grid>
+    </div>
+  )
+}
+
+export default CheckoutPage;
